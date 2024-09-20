10af957f9ae47f45ab4720f9fe272f32d9724e66
sh95fit <h95fit@gmail.com>
2024-09-20 18:05:44 +0900

    프로젝트 시작 전 React Refresh - Create 기능 구현 실습 (concat을 활용한 원본 유지)

--- frontend/ICT_PMS/src/App.jsx
+++ frontend/ICT_PMS/src/App.jsx
@@ -2,7 +2,9 @@
 import React, { Component } from 'react'
 import Subject from './components/Subject'
 import Nav from './components/Navbar'
-import Content from './components/Content'
+import ReadContent from './components/ReadContent'
+import Create from './components/Create'
+import CreateContent from './components/CreateContent'
 import './App.css'
 
 
@@ -10,6 +12,7 @@ class App extends Component {
   // render보다 먼저 실행되면서 컴포넌트를 초기화해주는 역할
   constructor(props){
     super(props);
+    this.max_content_id = 3;
     this.state = {
       mode: "homepage",
       selected_content_id:1,
@@ -24,10 +27,11 @@ class App extends Component {
   }
 
   render() {
-    var _title, _desc = null;
+    var _title, _desc, _content = null;
     if(this.state.mode === 'homepage') {
       _title = this.state.homepage.title;
       _desc = this.state.homepage.desc;
+      _content = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read'){
       var i = 0;
       while(i<this.state.contents.length){
@@ -39,6 +43,32 @@ class App extends Component {
         }
         i = i + 1;
       }
+      _content = <ReadContent title={_title} desc={_desc}></ReadContent>
+    } else if(this.state.mode === 'create'){
+      _content = <CreateContent onSubmit={function(_title, _desc){
+        // console.log(_title, _desc)
+        this.max_content_id = this.max_content_id+1;
+
+        // 원본을 수정하지 않는다 == 불변 (immutable)
+
+
+        // push는 원본을 변경시킨다
+        // this.state.contents.push({id:this.max_content_id, title:_title, desc:_desc});
+
+        // concat : 원본을 그대로 두고 복제해서 변경시킨다.
+        // var _content = this.state.contents.concat({id:this.max_content_id, title:_title, desc:_desc});
+
+        // Array.from(배열) : 배열 복제      // 객체를 복제하고 싶은 경우 : Object.assign({},객체);
+        // Immutable을 활용하여 원본을 유지하고 복제본을 활용하는 방법도 유효
+        var _content = Array.from(this.state.contents);
+        _content.push({id:this.max_content_id, title:_title, desc:_desc});
+
+        this.setState({
+          contents:_content
+        })
+
+
+      }.bind(this)}></CreateContent>
     }
 
 
@@ -48,19 +78,36 @@ class App extends Component {
           title={this.state.subject.title}
           sub={this.state.subject.sub}
           onChangePage={function(){
-            this.setState({mode:'homepage'});
+            this.setState({mode:'homepage'});  // 원본을 변경한 것
           }.bind(this)}
         ></Subject>
         <Nav data={this.state.contents} onChangePage={function(id){
           this.setState({
             mode:'read',
-            selected_content_id:Number(id),
+            selected_content_id:Number(id)
           })
         }.bind(this)}></Nav>
-        <Content title={_title} desc={_desc}></Content>
+        <Create onChangeMode ={function(mode) {
+          this.setState({
+            mode: mode
+          })
+        }.bind(this)}></Create>
+        { _content }
       </div>
     );
   }
 }
 
 export default App;
+
+
+// Props vs State
+
+// Props : read-only, can not be modified
+// State : change can be asynchronous, can be modified using this.setState
+
+// 컴포넌트 내에서의 props 수정은 불가능하다
+// 컴포넌트 외부에서는 props 값을 수정할 수 있다
+
+// 두 값의 변화는 render 함수 호출을 유발한다 (UI 변경 시 적절히 사용)
+
--- frontend/ICT_PMS/src/components/Navbar.jsx
+++ frontend/ICT_PMS/src/components/Navbar.jsx
@@ -1,6 +1,25 @@
 import React, { Component } from 'react';
 
 class Nav extends Component {
+  shouldComponentUpdate(newProps, newState) {
+    // newProps.data와 this.props.data 값이 동일
+
+    // 중요! state 원본을 변경하면 두 값이 함께 변경된다
+    // 즉, push 함수는 원본을 변경하므로 아래의 비교를 사용할 수 없다
+    // concat과 같이 복제본을 통해 변경시킨 경우 비교를 사용할 수 있다
+
+    // 부모 컴포넌트의 state가 변경될 때 newProps는 수정이 반영되고 this.props.data에는 반영되지 않는다
+    // 즉, newProps는 변경이 발생한 값, this.props.data는 변경되기 전 값을 의미
+
+    if(newProps.data === this.props.data){
+      return false;
+    }
+    return true;
+
+    // return true; // render 함수를 정상적으로 호출한다.
+    // return false; // render 함수를 호출하지 않는다.
+  }
+
   render() {
     var lists = [];
     var data = this.props.data;
